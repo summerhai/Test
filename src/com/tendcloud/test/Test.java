d673bb5aacb65b3dc6704c4989f50d470cec9047
tendcloud <hanlaiming@tju.edu.cn>
2015-07-21 14:45:22 +0800

    添加获取ip和主机名

--- src/com/tendcloud/test/Test.java
+++ src/com/tendcloud/test/Test.java
@@ -1,7 +1,14 @@
 package com.tendcloud.test;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Test {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
 		System.out.println("test");
+		InetAddress addr = InetAddress.getLocalHost();
+		String ip=addr.getHostAddress().toString();//获得本机IP
+		String address=addr.getHostName().toString();//获得本机名称
+		System.out.println(ip+","+address);
 	}
 }
